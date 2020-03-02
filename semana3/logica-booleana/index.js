ffcf86850750ec5db8066d80e7f98aea1f1063b3
Ricardo Araujo <r_araujo_pereira@hotmail.com>
2020-03-02 18:42:51 -0300

    exercício e desafío semana3-aula1

--- semana3/logica-booleana/index.js
+++ semana3/logica-booleana/index.js
@@ -44,7 +44,7 @@ VI.  3
 VII.  1
 */
 
-
+/*
 //a. Calcule e mostre o valor de 77°F em  K, mostrando a unidade no console também. 
 //(KELVIN) = (GRAUS_FAHRENHEIT - 32)*5/9 + 273.15
 
@@ -66,4 +66,94 @@ tC=30;
 const celsiusToFahrenheit2 = ((tC*(9/5))+32);
 const celsiusToKelvin= (tC + 273.15)
 console.log(celsiusToFahrenheit2 + "ºF" + " e " + celsiusToKelvin + "ºK");
+
+//d. Altere o último item para que o usuário insira o valor em graus Celsius que ele deseja converter
+tC = prompt();
+const celsiusToFahrenheit3 = ((tC*(9/5))+32);
+const celsiusToKelvin2= (tC + 273.15)
+console.log(celsiusToFahrenheit3 + "ºF" + " e " + celsiusToKelvin2 + "ºK");
+
+*/
+
+
+//2. perguntas para o usuário
+pergunta1 = prompt("Oi, quer ser meu amigx ? Eu posso só parecer uma página de computador estranha, mas sou legal");
+console.log ("Oi, quer ser meu amigx ? Eu posso só parecer uma página de computador estranha, mas sou legal!")
+console.log (pergunta1);
+
+pergunta2 = prompt("Independente do que respondeu, vou considerar que sim :). Qual o seu nome, novo amigx ?");
+console.log ("Independente do que respondeu, vou considerar que sim :). Qual o seu nome, novo amigo ?");
+console.log ("Belo nome " + pergunta2);
+
+pergunta3 = prompt("Você sabia que não podemos chamar o Javascript de java ?");
+console.log ("Você sabia que não podemos chamar o Javascript de java ?");
+console.log (pergunta3);
+
+pergunta4 = prompt("Eu não consegui entender muito bem o que você falou, pois hoje é meu primeiro dia de vida e ainda não tenho muita consciência. Mas quero te perguntar se você conhece o meu criador ?");
+console.log ("Eu não consegui entender muito bem o que você falou, pois hoje é meu primeiro dia de vida e ainda não tenho muita consciência. Mas quero te perguntar se você conhece o meu criador ?");
+console.log (pergunta4);
+
+pergunta5 = prompt("Novamente não consegui entender o que respondeu, mas não tem problema eu te conto. Fui criado por um aluno do curso FullStack da Future4, e em algumas semanas ele estará fazendo programas ainda mais incríveis, talvez até eu tenha um update. Um abraço e o meu até logo " +pergunta2);
+console.log ("Novamente não consegui entender o que respondeu, mas não tem problema eu te conto. Fui criado por um aluno do curso FullStack da Future4, e em algumas semanas ele estará fazendo programas ainda mais incríveis, talvez até eu tenha um update. Um abraço e o meu até logo " + pergunta2);
+console.log (pergunta5);
+
+
+
+/*
+//3. conta de consumo de energia.
+consumo = Number (prompt("digite o valor consumido em kwh"));
+
+let valor = consumo * 0.05;
+console.log("Valor a pagar R$" + valor);
+
+let valorDesconto = valor - (valor *0.15);
+console.log("Valor a pagar com desconto R$" + valorDesconto);
+*/
+
+
+
+/*
+//DESAFIO
+
+//libra = Number (prompt("digite o valor em libra que será convertido para kg"));
+libra = 20;
+const libraToKg = libra / 2.205;
+console.log(libra+ "lb equivale a " + libraToKg + "kg");
+
+
+//onca = Number (prompt("digite o valor em onças que será convertido para kg"));
+onca = 10.5;
+const oncaToKg = onca / 35.274;
+console.log(onca+ "oz equivale a " + oncaToKg + "kg");
+
+//milha = Number (prompt("digite o valor em milhas que será convertido para metros"));
+milha = 100;
+const milhasToMetros = milha * 1609.34;
+console.log(milha+ "mi equivale a " + milhasToMetros + "m");
+
+
+//pes = Number (prompt("digite o valor em pés que será convertido para metros"));
+pes = 100;
+const pesToMetro = pes / 3.281;
+console.log(pes+ "ft equivale a " + pesToMetro + "m");
+
+//galaos = Number (prompt("digite o valor em galões que será convertido para litros"));
+galoes = 103.56;
+const galoesToLitros = galoes * 3.78541;
+console.log(galoes+ "gal equivale a " + galoesToLitros + "l");
+
+//xicaras = Number (prompt("digite o valor em xicaras que será convertido para litros"));
+xicaras = 450;
+const xicarasToLitros = xicaras / 3.52;
+console.log(xicaras+ "xic equivale a " + xicarasToLitros + "l");
+
+*/
+
+
+
+
+
+
+
+
 
