5963be51b00b2eed03711ed46789bbb488bbc49c
Ricardo Araujo <r_araujo_pereira@hotmail.com>
2020-03-11 17:01:19 -0300

    exercício - desafio1

--- semana4/blog/index.js
+++ semana4/blog/index.js
@@ -19,33 +19,48 @@
 //     }
 // }
 
-function publicar(){
-        const titulo = document.getElementById("titulo");
-        const conteudo = document.getElementById("conteudo");
-        const autor = document.getElementById("autor");
-        const areaposts = document.getElementById("posts");
-    
-        const objeto = new classDePost (titulo.value, conteudo.value, autor.value);
-    
-        let arrayObjeto = [objeto.titulo, objeto.conteudo,objeto.autor];
+function publicar() {
+    const titulo = document.getElementById("titulo");
+    const conteudo = document.getElementById("conteudo");
+    const imagem = document.getElementById("imagem");
+    const autor = document.getElementById("autor");
+    const areaposts = document.getElementById("posts");
 
+    const objeto = new classDePost(titulo.value, conteudo.value, imagem.value, autor.value);
 
-        areaposts.innerHTML+='<div class="titulo">' + arrayObjeto[0] + '</div>';
-        areaposts.innerHTML+='<div class="conteudo">' + arrayObjeto[1]+ '</div>';
-        areaposts.innerHTML+='<div class="autor">' + arrayObjeto[2]+ '</div>';
+    let arrayObjeto = [objeto.titulo, objeto.conteudo, objeto.autor];
 
-        titulo.value="";
-        conteudo.value="";
-        autor.value="";
-    }
-    
-    class classDePost{
-        constructor (a,b,c){
-            this.titulo=a;
-            this.conteudo=b;
-            this.autor=c;
+    areaposts.innerHTML += '<div class="titulo">' + arrayObjeto[0] + '</div>';
+    areaposts.innerHTML += '<div class="conteudo">' + arrayObjeto[1] + '</div>';
+
+    if (imagem.value !== "") {
+        if (imagem.value.includes(".jpg")) {
+            const corpo = document.querySelector("body");
+            areaposts.innerHTML += "<img src=" + imagem.value + ' alt="imagem" >';
+            imagem.value = "";
+
+        } else {
+            alert("Não é uma imagem");
+            imagem.value = "";
         }
+
     }
+
+    areaposts.innerHTML += '<div class="autor">' + arrayObjeto[2] + '</div>';
+
+    titulo.value = "";
+    conteudo.value = "";
+    autor.value = "";
+
+}
+
+class classDePost {
+    constructor(titulo, conteudo, imagem, autor) {
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+        this.imagem = imagem;
+        this.autor = autor;
+    }
+}
 
 //não entendi para que preciso do array.
-
