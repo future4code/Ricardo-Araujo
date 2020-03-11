cef80a0a43d20cba5d8010e75954466212fe1f5e
Ricardo Araujo <r_araujo_pereira@hotmail.com>
2020-03-11 16:06:56 -0300

    exercício - parte3 não entendi para que preciso da array

--- semana4/blog/index.js
+++ semana4/blog/index.js
@@ -1,21 +1,50 @@
+// function publicar(){
+//     const titulo = document.getElementById("titulo");
+//     const conteudo = document.getElementById("conteudo");
+//     const autor = document.getElementById("autor");
+//     const areaposts = document.getElementById("posts");
+
+//     const objeto = new classDePost (titulo.value, conteudo.value, autor.valeu);
+
+//     areaposts.innerHTML+='<div class="titulo">' + objeto.titulo + '</div>';
+//     areaposts.innerHTML+='<div class="conteudo">' + objeto.conteudo+ '</div>';
+//     areaposts.innerHTML+='<div class="autor">' + objeto.autor+ '</div>';
+// }
+
+// class classDePost{
+//     constructor (a,b,c){
+//         this.titulo=a;
+//         this.conteudo=b;
+//         this.autor=c;
+//     }
+// }
+
 function publicar(){
-    const titulo = document.getElementById("titulo");
-    const conteudo = document.getElementById("conteudo");
-    const autor = document.getElementById("autor");
-    const areaposts = document.getElementById("posts");
+        const titulo = document.getElementById("titulo");
+        const conteudo = document.getElementById("conteudo");
+        const autor = document.getElementById("autor");
+        const areaposts = document.getElementById("posts");
+    
+        const objeto = new classDePost (titulo.value, conteudo.value, autor.value);
+    
+        let arrayObjeto = [objeto.titulo, objeto.conteudo,objeto.autor];
 
-    const objeto = new classDePost (titulo.value, conteudo.value, autor.valeu);
 
-    areaposts.innerHTML+='<div class="titulo">' + objeto.titulo + '</div>';
-    areaposts.innerHTML+='<div class="conteudo">' + objeto.conteudo+ '</div>';
-    areaposts.innerHTML+='<div class="autor">' + objeto.autor+ '</div>';
-}
+        areaposts.innerHTML+='<div class="titulo">' + arrayObjeto[0] + '</div>';
+        areaposts.innerHTML+='<div class="conteudo">' + arrayObjeto[1]+ '</div>';
+        areaposts.innerHTML+='<div class="autor">' + arrayObjeto[2]+ '</div>';
 
-class classDePost{
-    constructor (a,b,c){
-        this.titulo=a;
-        this.conteudo=b;
-        this.autor=c;
+        titulo.value="";
+        conteudo.value="";
+        autor.value="";
     }
-}
+    
+    class classDePost{
+        constructor (a,b,c){
+            this.titulo=a;
+            this.conteudo=b;
+            this.autor=c;
+        }
+    }
 
+//não entendi para que preciso do array.
