cd032a046b947b664e9e0010b989b72949f85ce9
Ricardo Araujo <r_araujo_pereira@hotmail.com>
2020-04-01 16:54:41 -0300

    semana7-aula3 - exercicio e parte dos desafios

--- semana7/integracao-api/src/App.js
+++ semana7/integracao-api/src/App.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components'
 import Form from './components/Form'
 import Users from './components/Users'
+
+const ContainerApp = styled.div`
+
+`
+const BotaoVoltar = styled.button`
+  height: 20px;
+  width: 200px;
+  background-color: yellow;
+`
+
 
 
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state={
-      qualTela: true
+      qualTela: true,
+      usuarios:[]
     }
   }
+
+componentDidMount(){
+  this.pegaDadoServidor()
+}
+
+pegaDadoServidor=()=>{
+  axios.get("https://us-central1-future-apis.cloudfunctions.net/api/users", {headers: {
+      "api-token": "ricardo-hamilton"
+  } 
+  }   
+  ).then(response=>{
+      this.setState({usuarios: response.data.result})
+  })
+  .catch(error=>{
+      alert("Não foi possivel pegar os dados do servidor")
+  })
+}
 
 mudaTela = ()=>{
   this.setState({qualTela: !this.state.qualTela})
@@ -19,15 +48,21 @@ mudaTela = ()=>{
 
 render(){
   return (
-    <div className="App">
+    <ContainerApp>
 
       {this.state.qualTela ?
-      <Form><button onClick={this.mudaTela}>Ir para página de lista</button></Form> : 
-      <Users><button onClick={this.mudaTela}>Ir para página de registro</button></Users>}
+      <Form><BotaoVoltar onClick={this.mudaTela}>Ir para página de lista</BotaoVoltar></Form> : 
+      <Users usuarios={this.state.usuarios} 
+        pegaDadoServidor={this.pegaDadoServidor}>
+        <BotaoVoltar onClick={this.mudaTela}>Ir para página de registro</BotaoVoltar>
+      </Users>}
       
-    </div>
+    </ContainerApp>
   );
 }
 }
 
 export default App;
+
+
+//screeching-cook.surge.sh
--- semana7/integracao-api/src/components/Users.js
+++ semana7/integracao-api/src/components/Users.js
@@ -1,25 +1,102 @@
 import React from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+import UserDetails from './UserDetails'
+
+const ContainerUsers = styled.div`
+    display:flex;
+    flex-direction: column;
+`
+
+const ContainerLista = styled.div`
+    margin:0 auto;
+    margin-top:100px;
+    display: flex;
+    flex-direction:column;
+`
+const CadaUsuario = styled.div`
+    display:flex;
+    justify-content:space-between;
+`
+const DetalheUsuario = styled.div`
+    background-color: pink;
+`
 
 class Users extends React.Component{
     constructor(props){
         super(props)
         this.state={
-
+            detalheUsuario: ""
         }
     }
 
+componentDidMount(){
+    {this.props.pegaDadoServidor()}
+}    
+
+renderizaDados=()=>{
+    const auxiliar=this.props.usuarios.map((elemento)=>{
+        return (<CadaUsuario key={elemento.id}>
+                    <span onClick={()=>this.mostraUsuario(elemento.id)}>{elemento.name}</span>
+                    <span onClick={()=>this.deletaUsuario(elemento.id)}>X</span>
+                </CadaUsuario> )
+    })
+    return auxiliar;
+}
+
+deletaUsuario=(id)=>{
+    if (window.confirm('Tem certeza que deseja apagar esta mensagem?')) {
+    axios
+        .delete(
+        `https://us-central1-future-apis.cloudfunctions.net/api/users/${id}`,
+        {
+            headers: {
+            "api-token": "ricardo-hamilton"
+            }
+        }
+        ).then(()=>{
+            {this.props.pegaDadoServidor()}
+            alert("Usuário deletado com sucesso")
+        }).catch(()=>{
+            alert("Algo deu errado, usuário não deletado")
+        })
+    }
+}
+
+
+mostraUsuario=(id)=>{
+
+axios.get(`https://us-central1-future-apis.cloudfunctions.net/api/users/${id}`,
+{
+    headers: {
+    "api-token": "ricardo-hamilton"
+    }
+}
+).then((response)=>{
+    this.setState({detalheUsuario: response.data.result})
+}).catch(()=>{
+    alert("algo deu errado");
+})
+
+}
+
+
+
 render(){
     return(
-        <div>
+        <ContainerUsers>
             {this.props.children}
-            Estou na lista
-        </div>
+            <ContainerLista>
+                <h2>Usuários Cadastrados:</h2>
+                {this.renderizaDados()}
+            </ContainerLista>
 
+            <DetalheUsuario>
+                <UserDetails detalheUsuario={this.state.detalheUsuario}/>
+            </DetalheUsuario>
+
+        </ContainerUsers>
     )
-
-
-
-
 }
 
 
