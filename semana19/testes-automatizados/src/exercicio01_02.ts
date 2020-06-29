6eae46d8fa9ddcffd97050209b29e8b15f0752d5
araujoricardo <r_araujo_pereira@hotmail.com>
2020-06-29 17:09:43 -0300

    semana19-aula1

---
+++ semana19/testes-automatizados/src/exercicio01_02.ts
@@ -0,0 +1,16 @@
+
+export interface User {
+	name: string
+	balance: number
+};
+
+export function performPurchase(user: User, value: number): User | undefined {
+	if(user.balance >= value) {
+		return {
+			...user,
+			balance: user.balance - value		
+		}
+	}
+	return undefined
+};
+
